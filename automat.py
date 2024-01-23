4cf975fb727f455c820015a4f953b4037120050b
Kamil <kamiltomaszek@icloud.com>
2024-01-23 14:08:05 +0100

    Refactor bnp_wpis function to improve readability and fix indexing bug

--- automat.py
+++ automat.py
@@ -256,7 +256,7 @@ def bnp_wpis(plik, poz1, poz2):
     n = 0
     started = None
     for index, row in enumerate(tab):
-        if started != None and index >= started + ile:
+        if started != None and index >= started + ile + 1:
             break
         if poz1 in skip2:
             poz1 += 1
@@ -266,7 +266,7 @@ def bnp_wpis(plik, poz1, poz2):
                 n = 1
                 started = index + 1
             if n == 1:
-                print(index - started + 2, ': ',  started + ile, 'z ',  f'({index+1})', row)
+                print(f'{index - started + 2}: {index+1} z {started + ile} --- {row}')
                 if len(row) == 1:
                     if row[0] != 'N/A':
                         if row[0] in dic:
