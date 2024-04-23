de31bafc7bd53ee0ff30ff847e0a29cc5ba3c984
Kamil <kamiltomaszek@icloud.com>
2024-04-23 14:36:34 +0200

    Add .exe  to .gitignore and update file renaming in nazwy.py

--- porownania/nazwy_faktur/nazwy.py
+++ porownania/nazwy_faktur/nazwy.py
@@ -1,6 +1,7 @@
-from pypdf import PdfReader
+import pdfminer.high_level
 import os
 import re
+import pdfminer
 
 NUM_FAK = r'FS/\d{2}/\d{2}/\d{5}'
 NUM_REZ = r'1\d{6}'
@@ -9,7 +10,8 @@ def main():
     for file in os.listdir('.'):
         if file.endswith('.pdf'):
             with open(file, 'rb') as f:
-                text = PdfReader(f).pages[0].extract_text()
+                text = pdfminer.high_level.extract_text(f)
+                print(text)
             num_fak = re.search(NUM_FAK, text).group().replace('/', '_')
             num_rez = re.search(NUM_REZ, text).group()
             os.rename(file, f'./zmienione/{num_fak} rez. {num_rez}.pdf')
