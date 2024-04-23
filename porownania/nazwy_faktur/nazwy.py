cf78ca6c5beb884f9cc5f0413b8b00b1da9caaab
Kamil <kamiltomaszek@icloud.com>
2024-04-23 12:28:18 +0200

    zamiana nazw faktur

---
+++ porownania/nazwy_faktur/nazwy.py
@@ -0,0 +1,19 @@
+from pypdf import PdfReader
+import os
+import re
+from shutil import move
+
+NUM_FAK = r'FS/\d{2}/\d{2}/\d{5}'
+NUM_REZ = r'1\d{6}'
+
+def main():
+    for file in os.listdir('.'):
+        if file.endswith('.pdf'):
+            with open(file, 'rb') as f:
+                text = PdfReader(f).pages[0].extract_text()
+            num_fak = re.search(NUM_FAK, text).group().replace('/', '_')
+            num_rez = re.search(NUM_REZ, text).group()
+            move(file, f'./zmienione/{num_fak} rez. {num_rez}.pdf')
+
+if __name__ == '__main__':
+    main()
