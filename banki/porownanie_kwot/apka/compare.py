d3e06530e59ad459ab1ae86f992680541216a38e
Kamil <kamiltomaszek@icloud.com>
2024-10-05 18:14:58 +0200

    cleaning up the folders

---
+++ banki/porownanie_kwot/apka/compare.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def read_xlsx(file):
+    df = pd.read_excel(file, header=0)
+    headers = df.columns.tolist()
+    df1 = df.iloc[:, 0].tolist()
+    df2 = df.iloc[:, 1].tolist()
+    return headers, df1, df2
+
+def match_lists(list1, list2):
+    matches = []
+    unmatched_list1 = list1.copy()
+    unmatched_list2 = list2.copy()
+    
+    for i in list1:
+        if i in list2:
+            matches.append((i, i))
+            unmatched_list1.remove(i)
+            unmatched_list2.remove(i)
+    
+    return matches, unmatched_list1, unmatched_list2
+
+def save_to_excel(headers, exact_matches, unmatched_list1, unmatched_list2):
+    df_exact = pd.DataFrame(exact_matches, columns=headers)
+    unmatched_data = {headers[0]: unmatched_list1 + [None] * (len(unmatched_list2) - len(unmatched_list1)),
+                      headers[1]: unmatched_list2 + [None] * (len(unmatched_list1) - len(unmatched_list2))}
+    df_unmatched = pd.DataFrame(unmatched_data)
+
+    with pd.ExcelWriter('comparison_output.xlsx') as writer:
+        df_exact.to_excel(writer, sheet_name='Exact Matches', index=False)
+        df_unmatched.to_excel(writer, sheet_name='Unmatched', index=False)
+        
+    print('Comparison results saved to comparison_output.xlsx')
+
+def main():
+    file = 'compare.xlsx'
+    headers, list1, list2 = read_xlsx(file)
+
+    exact_matches, unmatched_list1, unmatched_list2 = match_lists(list1, list2)
+
+    save_to_excel(headers, exact_matches, unmatched_list1, unmatched_list2)
